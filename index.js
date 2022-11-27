e7aa0011ac0424589b6464aa9648de9c1e03bb21
Sachin Subugade <sachinrs1310@gmail.com>
2022-11-27 19:36:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const cardDetails = {
+  name: "Sachin Subugad",
+  cardNo: "1234 5678 9012",
+  expiry: "12/25",
+  cvv: 123,
+};
+
+const cardNo = document.getElementById("cardNoNo");
+cardNo.innerText = cardDetails.cardNo;
